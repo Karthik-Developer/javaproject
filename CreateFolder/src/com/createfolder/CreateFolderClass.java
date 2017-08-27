d30e8d9cf773631044cb467f9749c78f5cc3c21b
karthik-developer <kart.sv@gmail.com>
2017-08-26 23:24:23 -0700

    Committed

--- CreateFolder/src/com/createfolder/CreateFolderClass.java
+++ CreateFolder/src/com/createfolder/CreateFolderClass.java
@@ -1,10 +1,24 @@
 package com.createfolder;
 
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class CreateFolderClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World"); 
+		System.out.println(""+Calendar.DAY_OF_YEAR);
+		LocalDateTime timeDate = LocalDateTime.now();
+		System.out.println(timeDate.getYear()+"-"+timeDate.getMonth()+"-"+timeDate.getDayOfMonth());
+		
+		Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+        
+        System.out.println(year+"-"+month+"-"+day);
+		
 	}
 
 }
